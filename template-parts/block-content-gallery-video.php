856a6de0376d705f9259539e0469b0c5436c0789
Yury <moiseev.yu@yandex.ru>
2025-03-01 15:53:25 +0300

    Измнено добавление видео через rutube

--- template-parts/block-content-gallery-video.php
+++ template-parts/block-content-gallery-video.php
@@ -6,25 +6,42 @@
 ?>
 
 <ul class="allery-tab-target__list gallery-tab-list">
-    <?php  
-    if( have_rows('add_video_block') ): ?>
-    <?php $i = 1; while( have_rows('add_video_block') ): the_row(); 
-    $gallery_content = get_sub_field('gallery_video_content'); 
-    $index = $i++;            
-    ?>
+    <?php
+    if (have_rows('add_video_block')): ?>
+        <?php $i = 1;
+        while (have_rows('add_video_block')):
+            the_row();
+            $gallery_content = get_sub_field('gallery_video_content');
+            $index = $i++;
+            ?>
 
-        <li class="gallery-tab-list__item gallery-tab-list__item_1 gallery-tab-list__video">
-            <div class="gallery-tab-list__image">
-                <div class="youtube-player" data-id="<?php echo $gallery_content['second']; ?>"></div>
-            </div>
-            <div class="gallery-tab-list__image">                                    
-                <div class="youtube-player" data-id="<?php echo $gallery_content['first']; ?>"></div>
-            </div>
-            <div class="gallery-tab-list__image">                                    
-                <div class="youtube-player" data-id="<?php echo $gallery_content['third']; ?>"></div>
-            </div>
-        </li>
+            <li class="gallery-tab-list__item gallery-tab-list__item_1 gallery-tab-list__video">
+                <?php if ($gallery_content['first']) { ?>
+                    <div class="gallery-tab-list__image">
+                        <iframe width="720" height="344" src="https://rutube.ru/play/embed/<?php echo $gallery_content['first']; ?>"
+                            frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen
+                            allowFullScreen>
+                        </iframe>
+                    </div>
+                <?php }
+                if ($gallery_content['second']) { ?>
+                    <div class="gallery-tab-list__image">
+                        <iframe width="720" height="344"
+                            src="https://rutube.ru/play/embed/<?php echo $gallery_content['second']; ?>" frameBorder="0"
+                            allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen>
+                        </iframe>
+                    </div>
+                <?php }
+                if ($gallery_content['third']) { ?>
+                    <div class="gallery-tab-list__image">
+                        <iframe width="720" height="344" src="https://rutube.ru/play/embed/<?php echo $gallery_content['third']; ?>"
+                            frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen
+                            allowFullScreen>
+                        </iframe>
+                    </div>
+                <?php } ?>
+            </li>
 
-    <?php endwhile; ?>
+        <?php endwhile; ?>
     <?php endif; ?>
 </ul>
--- template-parts/block-one-video.php
+++ template-parts/block-one-video.php
@@ -1,11 +1,11 @@
 <?php
 /**
-* Display Block width one video
-* Блок отображает одно видео + текст
-*/
+ * Display Block width one video
+ * Блок отображает одно видео + текст
+ */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
 }
 $page_id = get_the_ID();
 $one_video_title = get_field('one_video_title', $page_id);
@@ -15,40 +15,43 @@ $one_video_type = get_field('one_video_type', $page_id);
 $one_video = get_field('one_video', $page_id);
 $one_video_link = get_field('one_video_link', $page_id);
 
-if($one_video || $one_video_link ) { 
-?>
+if ($one_video || $one_video_link) {
+    ?>
 
     <section class="dark one-video">
         <div class="container">
-            <div class="head-stripes-wrap" >
+            <div class="head-stripes-wrap">
                 <h2 class="head-stripes get-price__title">
                     <?php echo
-                    $actions_block_title ? $actions_block_title : 'Видео о нас';
-                    ?>                     
-                </h2> 
-            </div>               
+                        $actions_block_title ? $actions_block_title : 'Видео о нас';
+                    ?>
+                </h2>
+            </div>
         </div>
 
         <div class="container">
             <div class="one-video__wrap one-video-wrap d-grid align-items-center">
-                <div class="one-video-wrap__video"> 
+                <div class="one-video-wrap__video">
                     <?php
-                    if( $one_video_type == 'файл' && $one_video ) { ?>
+                    if ($one_video_type == 'файл' && $one_video) { ?>
                         <video controls class="one-video-wrap__file">
-                            <source src="<?php echo esc_url( $one_video['url'] ); ?>" type="video/webm" />
-            
-                            <source src="<?php echo esc_url( $one_video['url'] ); ?>" type="video/mp4" />
+                            <source src="<?php echo esc_url($one_video['url']); ?>" type="video/webm" />
+
+                            <source src="<?php echo esc_url($one_video['url']); ?>" type="video/mp4" />
                         </video>
-            
-                        <?php } else if( $one_video_type == 'ссылка' && $one_video_link ) { ?>
-                            <div class="youtube-player" data-id="<?php echo $one_video_link; ?>"></div>
-                    <?php } ?> 
+
+                    <?php } else if ($one_video_type == 'ссылка' && $one_video_link) { ?>
+                            <iframe width="720" height="344" src="https://rutube.ru/play/embed/<?php echo $one_video_link; ?>"
+                                frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen
+                                allowFullScreen>
+                            </iframe>
+                    <?php } ?>
                 </div>
 
                 <div class="one-video__text actions-item__title">
                     <p>
                         <?php echo $one_video_text; ?>
-                    </p>                   
+                    </p>
                 </div>
             </div>
         </div>
